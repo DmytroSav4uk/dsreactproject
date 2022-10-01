594cf091007124b55721e6f1e645092794911239
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-01 22:33:03 +0300

    i did smthg

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import {Link, Route, Routes} from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Todos from "./Components/Todos/Todos/Todos";
-import Albums from "./Components/Albums/Albums/Albums";
-import Comments from "./Components/Comments/Comments/Comments";
-import Posts from "./Components/Post/Posts/Posts";
-import css from "./App.module.css"
-
+import Dogs from "./Components/Dogs";
 
 function App() {
     return (
         <div >
-            <div className={css.controller}>
-            <ul>
-                <li><Link to={'/'}>Home Page</Link></li>
-                <li><Link to={'/todos'}>all todos</Link></li>
-                <li><Link to={'/albums'}>all albums</Link></li>
-                <li><Link to={'/comments'}>all comments</Link></li>
-            </ul>
-            </div>
-
-            <div className={css.main}>
-                <Routes>
-                    <Route path={'/'} element={<Home/>}/>
-                    <Route path={'/todos'} element={<Todos/>}/>
-                    <Route path={'/albums'} element={<Albums/>}/>
-                    <Route path={'/comments'} element={<Comments/>}/>
-                    <Route path={'/comments/:id/posts'} element={<Posts/>}/>
-                </Routes>
-            </div>
-
+<Dogs/>
         </div>)
 }
 

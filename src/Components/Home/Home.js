d171ce6c47ff8d5765564290269d9c484318347b
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-01 19:35:33 +0300

    initial commit

--- src/Components/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import css from "../CSS/components.module.css"
-
-
-export default function Home (){
-    return(
-        <div className={css.home}>
-            Welcome to homework №5
-        </div>)}
--- src/Configs/urls.js
+++
@@ -1,3 +0,0 @@
-const baseURL = 'https://jsonplaceholder.typicode.com';
-
-export {baseURL};

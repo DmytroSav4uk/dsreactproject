c2b013a8e421c3a830fc033258b704d945b9ddd2
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-02 16:15:16 +0300

    finished but need to improve code

--- src/Components/Cats/Cats.js
+++ src/Components/Cats/Cats.js
@@ -1,9 +1,19 @@
 import {useEleReducer} from "../../Reducers/reducers";
 import {CREATE, DELETE} from "../../Reducers/actions";
-import {useRef} from "react";
+import {useRef,useState} from "react";
 import {Cat} from "./Cat";
 
+
 export default function Cats() {
+
+
+    const [value, setValue] = useState('');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+
 
     const [state, dispatch] = useEleReducer();
 
@@ -11,18 +21,19 @@ export default function Cats() {
 
     const createCat = () => {
         dispatch({type: CREATE, payload: {name: catName.current.value}})
-        catName.current.value ="";
     }
 
     const deleteCat = (id) => {
         dispatch({type: DELETE, payload:{id}})
     }
 
+
+
     return(
         <div>
             <p> Cats</p>
-            <input type="text" ref={catName} />
-            <button onClick={createCat}>Add</button>
+            <input type="text"   value={value} onChange={handleChange} ref={catName} />
+            <button disabled={!value} onClick={createCat}>Add</button>
             <div>{state.map((cat, index) => <Cat key={index} cat={cat} deleteCat={deleteCat}/>)}</div>
         </div>
     )
--- src/Components/Dogs/Dogs.js
+++ src/Components/Dogs/Dogs.js
@@ -1,21 +1,27 @@
 import {Dog} from "./Dog";
 import {useEleReducer} from "../../Reducers/reducers";
 import {CREATE, DELETE} from "../../Reducers/actions";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 
 
 export default function Dogs (){
 
+    const [value, setValue] = useState('');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
     const [state, dispatch] = useEleReducer();
 
-    let dogName = useRef()
+    let dogName = useRef(null)
 
 
 
 
 const createDog = () => {
     dispatch({type: CREATE, payload: {name: dogName.current.value}})
-    dogName.current.value ="";
+    dogName.current.value = '';
 }
 
 const deleteDog = (id) => {
@@ -23,13 +29,11 @@ const deleteDog = (id) => {
 }
 
 
-
-
     return(
         <div>
             <p>Dogs</p>
-            <input type="text" ref={dogName} />
-            <button onClick={createDog}>Add</button>
+            <input type="text" ref={dogName}  value={value} onChange={handleChange} />
+            <button disabled={!value} onClick={createDog}>Add</button>
             <div>{state.map((dog, index) => <Dog key={index} dog={dog} deleteDog={deleteDog}/>)}</div>
         </div>
     )
